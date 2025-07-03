57a3caa5194161dc4ca42255bac3e1d3ab7e84b1
Farid Shahidi <farid.sh69@gmail.com>
2025-07-03 10:02:06 +0200

    finished implementing resource structure in all cruds

--- src/main/java/com/farid/Divar/Controllers/UserController.java
+++ src/main/java/com/farid/Divar/Controllers/UserController.java
@@ -1,17 +1,17 @@
 package com.farid.Divar.Controllers;
 
 import com.farid.Divar.Library.ApiResource;
-import com.farid.Divar.Library.ResourceAbstract;
+import com.farid.Divar.Requests.UserRequest;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.farid.Divar.Configs.AppConfig;
 import com.farid.Divar.Models.User;
 import com.farid.Divar.Repositories.UserRepository;
 import com.farid.Divar.Resources.UserResource;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -22,10 +22,15 @@ public class UserController {
 		this.repository = repository;
 	}
 
-//	@GetMapping("/users")
-//	public ApiResource<List<UserResource>> index() {
-//		return new ApiResource<>(repository.findAll().stream().map((entity) -> {return new UserResource(entity);}).toList());
-//	}
+	@GetMapping("/users")
+	public ResponseEntity<ApiResource<List<UserResource>>> index() {
+		List<User> users = repository.findAll();
+		List<UserResource> userResources = users.stream()
+				.map(user -> UserResource.from(user, UserResource.class))
+				.toList();
+
+		return ResponseEntity.ok(new ApiResource<>(userResources));
+	}
 
 	@GetMapping("users/{id}")
 	public ResponseEntity<ApiResource<UserResource>> show(@PathVariable int id) {
@@ -35,31 +40,33 @@ public class UserController {
 		return ResponseEntity.ok(new ApiResource<>(resource));
 	}
 
-//	@PostMapping("/users")
-//	public ResponseEntity<ApiResource<UserResource>> create(@RequestBody User entity) {
-//		User savedEntity = repository.save(entity);
-//
-//		return ResponseEntity.ok(new ApiResource<>(new UserResource(savedEntity)));
-//	}
+	@PostMapping("/users")
+	public ResponseEntity<ApiResource<UserResource>> create(@RequestBody User entity) {
+		User savedEntity = repository.save(entity);
+		UserResource resource = UserResource.from(savedEntity, UserResource.class);
 
-//	@PutMapping("users/{id}")
-//	public ResponseEntity<ApiResource<UserResource>> update(@PathVariable int id, @RequestBody UserRequest updatingEntity) {
-//		return repository.findById(id).map(entity -> {
-//			entity.updateData(updatingEntity);
-//			repository.save(entity);
-//
-//			return ResponseEntity.ok(new ApiResource<>(new UserResource(entity)));
-//		}).orElseThrow(EntityNotFoundException::new);
-//	}
+		return ResponseEntity.ok(new ApiResource<>(resource));
+	}
 
-//	@DeleteMapping("/users/{id}")
-//	public ResponseEntity<ApiResource<UserResource>> delete(@PathVariable int id) {
-//		return repository.findById(id).map(entity -> {
-//			repository.delete(entity);
-//
-//			return ResponseEntity.ok(new ApiResource<>(new UserResource(entity)));
-//		}).orElseThrow(EntityNotFoundException::new);
-//	}
+	@PutMapping("users/{id}")
+	public ResponseEntity<ApiResource<UserResource>> update(@PathVariable int id, @RequestBody UserRequest updatingEntity) {
+		return repository.findById(id).map(entity -> {
+			entity.updateData(updatingEntity);
+			repository.save(entity);
+			UserResource resource = UserResource.from(entity, UserResource.class);
+
+			return ResponseEntity.ok(new ApiResource<>(resource));
+		}).orElseThrow(EntityNotFoundException::new);
+	}
+
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<ApiResource<User>> delete(@PathVariable int id) {
+		return repository.findById(id).map(entity -> {
+			repository.delete(entity);
+
+			return ResponseEntity.ok(new ApiResource<>(entity));
+		}).orElseThrow(EntityNotFoundException::new);
+	}
 
 	@Autowired
 	private AppConfig appConfig;
